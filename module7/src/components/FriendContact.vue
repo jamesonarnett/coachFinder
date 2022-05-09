029794954cabd6464df94a0c464b5c1ee7f08312
jamesonarnett <jamesonarnett@yahoo.com>
2022-05-09 16:34:29 -0500

    Module7 // vueCLI

---
+++ module7/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong> {{ friend.phone }}</li>
+      <li><strong>Email: </strong> {{ friend.email }}/li></li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "jameson",
+        name: "jameson arnett",
+        phone: "1233211233",
+        email: "another@email.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
---
+++ module7/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import FriendContact from "./components/FriendContact.vue";
+
+const app = createApp(App).mount("#app");
+
+app.component("friend-contact", FriendContact);
+app.mount("#app");
