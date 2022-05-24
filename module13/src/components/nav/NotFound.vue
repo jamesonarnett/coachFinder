0f43556662cf1fd82cf08b9921cdbaa1bb0cbb56
jamesonarnett <jamesonarnett@yahoo.com>
2022-05-23 19:21:20 -0500

    Adds catchAll route && / route

---
+++ module13/src/components/nav/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>
+    Page Not Found! Maybe you were looking for
+    <router-link to="/teams">Teams</router-link>?
+  </h2>
+</template>
+
+<style scoped>
+h2 {
+  text-align: center;
+}
+</style>
