5afe7699d6261ef80885ddce6d4da15b2b797857
jamesonarnett <jamesonarnett@yahoo.com>
2022-05-30 11:59:55 -0500

    State solo practice user'auth'

--- module15/src/App.vue
+++ module15/src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <base-container title="Vuex">
+  <base-container v-if="isAuth" title="Vuex">
     <the-counter></the-counter>
     <favorite-value></favorite-value>
     <button @click="addOne">Add 10</button>
     <change-counter></change-counter>
+  </base-container>
+
+  <base-container title="Auth">
+    <user-auth></user-auth>
   </base-container>
 </template>
 
@@ -12,6 +16,7 @@ import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
 import ChangeCounter from './components/ChangeCounter.vue';
 import FavoriteValue from './components/FavoriteValue.vue';
+import UserAuth from './components/UserAuth.vue';
 
 export default {
   components: {
@@ -19,6 +24,7 @@ export default {
     TheCounter,
     ChangeCounter,
     FavoriteValue,
+    UserAuth,
   },
   methods: {
     addOne() {
@@ -27,6 +33,11 @@ export default {
         type: 'increase',
         value: 10,
       });
+    },
+  },
+  computed: {
+    isAuth() {
+      return this.$store.getters.userIsAuthenticated;
     },
   },
 };
