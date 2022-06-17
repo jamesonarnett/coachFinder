358147bb138c9a6afe4e74740072ec382d5877b6
jamesonarnett <jamesonarnett@yahoo.com>
2022-06-16 19:24:08 -0500

    Adds functionality to registerCoach form

--- module16/src/pages/coaches/CoachRegistration.vue
+++ module16/src/pages/coaches/CoachRegistration.vue
@@ -2,7 +2,7 @@
   <section>
     <base-card>
       <h2>Register as a Coach</h2>
-      <coach-form></coach-form
+      <coach-form @save-data="saveData"></coach-form
     ></base-card>
   </section>
 </template>
@@ -12,5 +12,10 @@ import CoachForm from '../../components/layout/coaches/CoachForm.vue';
 
 export default {
   components: { CoachForm },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data);
+    },
+  },
 };
 </script>
--- module16/src/store/modules/coaches/actions.js
+++ module16/src/store/modules/coaches/actions.js
@@ -1,1 +1,15 @@
-export default {};
+export default {
+  registerCoach(context, data) {
+    const coachData = {
+      //hardcode id temporarily
+      id: 'c3',
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas,
+    };
+
+    context.commit('registerCoach', coachData);
+  },
+};
