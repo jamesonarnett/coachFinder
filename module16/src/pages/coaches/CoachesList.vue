9f18f3485d001a4acc2fbdf7bb77b9470e1f1975
jamesonarnett <jamesonarnett@yahoo.com>
2022-06-22 17:00:11 -0500

    Adds loadCoaches from firebase

--- module16/src/pages/coaches/CoachesList.vue
+++ module16/src/pages/coaches/CoachesList.vue
@@ -5,7 +5,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
+        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
         <base-button v-if="!isCoach" link to="/register"
           >Register as a Coach</base-button
         >
@@ -64,9 +64,15 @@ export default {
       },
     };
   },
+  created() {
+    this.loadCoaches();
+  },
   methods: {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
+    },
+    loadCoaches() {
+      this.$store.dispatch('coaches/loadCoaches');
     },
   },
 };
