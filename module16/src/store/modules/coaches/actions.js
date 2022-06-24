8d174ce0c1fec728bb6ea23d6aa53b704db58918
jamesonarnett <jamesonarnett@yahoo.com>
2022-06-24 17:55:13 -0500

    Caches coachList(60s), adds forceReload

--- module16/src/store/modules/coaches/actions.js
+++ module16/src/store/modules/coaches/actions.js
@@ -28,7 +28,11 @@ export default {
       id: userId,
     });
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
     const response = await fetch(
       `https://coachapp-9-default-rtdb.firebaseio.com/coaches.json`
     );
@@ -53,6 +57,7 @@ export default {
       coaches.push(coach);
 
       context.commit('setCoaches', coaches);
+      context.commit('setFetchTimestamp');
     }
   },
 };
