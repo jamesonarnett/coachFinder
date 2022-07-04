a628895acc94a3da7b08098e4353b121ee9f49ce
jamesonarnett <jamesonarnett@yahoo.com>
2022-07-03 19:14:51 -0500

    Adds autoLogin

--- module16/src/router.js
+++ module16/src/router.js
@@ -35,9 +35,9 @@ const router = createRouter({
 });
 
 router.beforeEach((to, _, next) => {
-  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+  if (to.meta.requiresAuth && !store.getters['auth/isAuthenticated']) {
     next('/auth');
-  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+  } else if (to.meta.requiresUnauth && store.getters['auth/isAuthenticated']) {
     next('/coaches');
   } else {
     next();
